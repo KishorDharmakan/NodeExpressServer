a2044aeccedb4463ddd9c08a26fa7fa20fb9de36
KishorAnandRajD <kishor.dharmakan@gmail.com>
2019-05-02 00:47:14 +0530

    included code to fix CORS

--- index.js
+++ index.js
@@ -16,10 +16,31 @@ https.get(url, res => {
   });
 });
 
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
+
 app.get("/userDetails", (req, res, next) => {
+    console.log('responseBody:', responseBody)
     res.json(responseBody);
+   // console.log('res:', res)
    });
 
 app.listen(3100, () => {
- console.log("Server running on port 3000");
+ console.log("Server running on port 3100");
 });
